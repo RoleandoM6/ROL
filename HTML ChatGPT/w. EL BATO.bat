e8d846847d2e302b35a5e5aeb395f37f15cacbd1
RolsixM6 <roleando.monsix@gmail.com>
2025-08-29 19:31:07 +0200

    fdsfdsfsd

---
+++ HTML ChatGPT/w. EL BATO.bat
@@ -0,0 +1,11 @@
+@echo off
+echo Ejecutando primer script...
+START "" "C:\Users\monsi\Desktop\ROL\HTML ChatGPT\x Arrancar Server LocalHost ROL.bat"
+
+echo Ejecutando segundo script...
+START "" "C:\Users\monsi\Desktop\ROL\HTML ChatGPT\x Mover y Sobreescribir JSON.bat"
+
+echo Ejecutando tercer script...
+START "" "C:\Users\monsi\Desktop\ROL\HTML ChatGPT\x AbrirChromeMax.bat"
+
+echo Todos los scripts ejecutados.
